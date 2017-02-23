0b5cb02d8caa580d1fb6a76e3d6915eb7cbcb15f
Bryan Wood <whenidev@gmail.com>
2017-02-23 10:07:31 -0800

    initial project

---
+++ src/AntiForgeryDemo/Middleware/AntiForgeryMiddleware.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Antiforgery;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AntiForgeryDemo.Middleware
+{
+    public class AntiForgeryMiddleware
+    {
+        private readonly RequestDelegate next;
+
+        public AntiForgeryMiddleware(RequestDelegate next)
+        {
+            this.next = next;
+        }
+
+        //public async Task Invoke(HttpContext context, IAntiforgery antiforgery)
+        public async Task Invoke(HttpContext context)
+        {
+            //if (string.Equals("GET", context.Request.Method, StringComparison.OrdinalIgnoreCase))
+            //{
+            //    var tokens = antiforgery.GetAndStoreTokens(context);
+            //    antiforgery.GetAndStoreTokens(context);
+
+            //    context.Response.Cookies.Append(tokens.CookieToken, tokens.RequestToken);
+            //}
+
+            context.Response.Cookies.Append("Hello", "World");
+
+            context.Response.Headers.Add("X-BRYAN", "Hello World");
+
+            await next.Invoke(context);
+        }
+    }
+}
---
+++ src/AntiForgeryDemo/Middleware/ApplicationBuilderExtensions.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Builder;
+
+namespace AntiForgeryDemo.Middleware
+{
+    public static class ApplicationBuilderExtensions
+    {
+        public static IApplicationBuilder UseAntiforgeryTokenMiddleware(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<AntiForgeryMiddleware>();
+        }
+    }
+}
