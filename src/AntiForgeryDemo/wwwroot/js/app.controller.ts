c001ac0df16822aea4533f1c4f111e45ec537821
Bryan Wood <whenidev@gmail.com>
2017-02-24 18:08:54 -0800

    Add typescript, test pages and setup for static files

---
+++ src/AntiForgeryDemo/wwwroot/js/app.controller.ts
@@ -0,0 +1,28 @@
+﻿namespace AntiForgeryToken {
+    export class AppController {
+        public requestSuccess: boolean = false;
+        public requestFailure: boolean = false;
+
+        static $inject = [
+            '$http'
+        ];
+
+        constructor(
+            private $http: ng.IHttpService
+        ) {
+
+        }
+
+        public saveData(): void {
+            let data: string = 'Hello  World';
+            let self = this;
+            this.$http.post('/api/values', data)
+                .then(function () {
+                    self.requestSuccess = true;
+                })
+                .catch(function () {
+                    self.requestFailure = true;
+                });
+        }
+    }
+}
---
+++ src/AntiForgeryDemo/wwwroot/js/app.ts
@@ -0,0 +1,5 @@
+﻿namespace AntiForgeryToken {
+    let module: ng.IModule = angular.module('app', []);
+
+    module.controller('AppController', AntiForgeryToken.AppController);
+}
