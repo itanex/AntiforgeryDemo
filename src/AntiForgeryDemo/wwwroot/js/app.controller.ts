2d5329883b155d1daad3e2e3d272ffd8b450a7b0
Bryan Wood <whenidev@gmail.com>
2017-02-24 18:25:48 -0800

    Correct issue with replay of button

--- src/AntiForgeryDemo/wwwroot/js/app.controller.ts
+++ src/AntiForgeryDemo/wwwroot/js/app.controller.ts
@@ -16,6 +16,10 @@
         public saveData(): void {
             let data: string = 'Hello  World';
             let self = this;
+
+            this.requestFailure = false;
+            this.requestSuccess = false;
+
             this.$http.post('/api/values', data)
                 .then(function () {
                     self.requestSuccess = true;
